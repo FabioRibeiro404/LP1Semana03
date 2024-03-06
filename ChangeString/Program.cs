ad3efbfdfb95142786751937922790af04a2f925
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-06 11:09:42 +0000

    Add character replacement

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,23 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insert a phrase: ");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Insert character to remove: ");
+            string remove = Console.ReadLine();
+            char verify = char.Parse(remove);
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (verify == phrase[i])
+                {
+                    Console.Write("X");
+                }
+                else
+                {
+                    Console.Write(phrase[i]);
+                }
+            }
         }
     }
 }
