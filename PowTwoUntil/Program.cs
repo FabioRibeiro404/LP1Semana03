5b0c7f6938d022d2219ba3b69f3b5c4b2a0f8d2e
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-06 17:10:58 +0000

    Add calling Methods 2 times in Main

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -6,7 +6,19 @@ namespace PowTwoUntil
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PowersOf2Until5();
+
+            PowersOf2Until5();
+        }
+
+
+
+        private static void PowersOf2Until5()
+        {
+            for (int i = 0; i <= (1 << 5); i = i << 1)
+                {
+                    Console.WriteLine(i);
+                }
         }
     }
 }
