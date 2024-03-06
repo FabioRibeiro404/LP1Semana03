a7149d6a2d7f62196067a589a7827ded0bf3d6c3
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-06 09:48:48 +0000

    Add minimun lenght verification

---
+++ RightSize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s in args)
+            {
+                if (s.Length > 3)
+                {
+                    Console.WriteLine(s);
+                }
+                else
+                    continue;
+            }
+        }
+    }
+}
