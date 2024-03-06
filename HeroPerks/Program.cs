a22ea2c00fdca229393dba0d4bb7f059f9849673
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-06 16:59:02 +0000

    Add all character verification and outputs

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -3,7 +3,7 @@
 namespace HeroPerks
 {
     [Flags]
-    enum HeroPerks
+    enum Perks
     {
         WaterBreathing = 1 << 0,
         Stealth = 1 << 1,
@@ -16,42 +16,62 @@ namespace HeroPerks
         static void Main(string[] args)
         {
             string playerPerks = "";
+            Perks MyClass = 0;
 
             foreach (string s in args)
             {
                 playerPerks = s;
             }
 
-            //Console.WriteLine(playerPerks);
-            int counterW = 0;
-            int counterS = 0;
-            int counterA = 0;
-            int counterD = 0;
+            
 
             for (int i = 0; i < playerPerks.Length; i++)
             {
                 if (playerPerks[i] == 'w')
                 {
-                    counterW++;
+                    MyClass ^= Perks.WaterBreathing;
                 }
                 else if (playerPerks[i] == 's')
                 {
-                    counterS++;
+                    MyClass ^= Perks.Stealth;
                 }
                 else if (playerPerks[i] == 'a')
                 {
-                    counterA++;
+                    MyClass ^= Perks.AutoHeal;
                 }
                 else if (playerPerks[i] == 'd')
-                {
-                    counterD++;
+                {   
+                    MyClass ^= Perks.DoubleJump;
                 }
                 else if (playerPerks[i] != 'w' || playerPerks[i] != 's'||
                     playerPerks[i] != 'a'|| playerPerks[i] != 'd')
                     {
                         Console.WriteLine("Unknown perk!");
+                        return;
                     }
             }
+            if (MyClass != 0)
+                Console.WriteLine(MyClass);
+
+            if ((MyClass & Perks.Stealth) == Perks.Stealth 
+            && (MyClass & Perks.DoubleJump) == Perks.DoubleJump)
+            {
+                Console.WriteLine("Silent jumper!");
+            }
+
+            if (MyClass == 0)
+            {
+                Console.WriteLine("No perks at all!");
+            }
+
+            if ((MyClass & Perks.AutoHeal) != Perks.AutoHeal)
+            {
+                Console.WriteLine("Not gonna make it!");
+            }
+            
+
+
+
         }
         
     }
